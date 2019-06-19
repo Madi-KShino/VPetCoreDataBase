6640b1ae1413be4743ab3f53234ee4c4e8c79598
Haley Jones <nyanidesuka@icloud.com>
2019-06-19 17:16:32 -0600

    Initial commit

---
+++ TamagotchiCoreData/Model Controller/VirtualPetController.swift
@@ -0,0 +1,25 @@
+//
+//  VirtualPetController.swift
+//  TamagotchiCoreData
+//
+//  Created by Haley Jones on 6/15/19.
+//  Copyright © 2019 HaleyJones. All rights reserved.
+//
+
+import Foundation
+
+class VirtualPetController{
+
+    /*
+    func updateStats(forPet pet: VirtualPet){
+        let currentDate = Date()
+        guard let petLastFed = pet.timeLastFed,
+            let petLastPet = pet.timeLastPet else {return}
+        let timeSinceFed = (currentDate.timeIntervalSince(petLastFed) / 60)
+        let timeSincePet = (currentDate.timeIntervalSince(petLastPet) / 100)
+        pet.hunger = min(100, pet.hunger + Int16(timeSinceFed))
+        pet.happiness = max(0, pet.happiness - Int16(timeSincePet))
+        VirtualPetController.shared.saveToPersistentStore()
+    }
+     */
+}
