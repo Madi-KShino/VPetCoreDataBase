2748c9e7ad7e6f6cc1f89abbd9cebcd0ec0132c5
Madi <madison.k.shino@gmail.com>
2020-01-12 12:34:35 -0700

    Views added

--- TamagotchiCoreData/Model Controller/PetsViewController.swift
+++ TamagotchiCoreData/Model Controller/PetsViewController.swift
@@ -12,23 +12,40 @@ class PetsViewController: UIViewController {
 
     //Outlets
     @IBOutlet weak var petListTableView: UITableView!
+    @IBOutlet weak var adoptNewPetButton: UIBarButtonItem!
     
     //Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setMainView()
+        setNavBarView()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //Actions
+    @IBAction func adoptButtonTapped(_ sender: Any) {
+        
+    }
+    
+    //Helper Functions
+    func setMainView() {
+        petListTableView.layer.cornerRadius = 25
+    }
+    
+    func setNavBarView() {
+        adoptNewPetButton.title = "Adopt"
+        let label = UILabel()
+        label.text = "My Pets"
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        label.textAlignment = .left
+        label.sizeToFit()
+        label.font = UIFont(name: "HelveticaNeue-Bold", size: 35)
+        navigationItem.titleView = label
+    }
+    
+    //MARK: - Navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
